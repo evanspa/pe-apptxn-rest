1824fcc743a501540917298416865671832dc1a8
Paul Evans <evansp2@gmail.com>
2015-03-21 00:06:10 -0400

    added release.sh and updating deps

--- project.clj
+++ project.clj
@@ -15,14 +15,14 @@
                   :exclusions [org.slf4j/slf4j-nop
                                joda-time
                                org.slf4j/slf4j-log4j12]]
-                 [pe-apptxn-core "0.0.1"]
-                 [pe-rest-utils "0.0.1"]
+                 [pe-apptxn-core "0.0.2"]
+                 [pe-rest-utils "0.0.2"]
                  [ch.qos.logback/logback-classic "1.0.13"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [clj-time "0.8.0"]]
   :resource-paths ["resources"]
   :codox {:exclude [user]
-          :src-dir-uri "https://github.com/evanspa/pe-apptxn-restsupport/blob/0.0.3/"
+          :src-dir-uri "https://github.com/evanspa/pe-apptxn-restsupport/blob/0.0.4/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:source-paths ["dev"]  ;ensures 'user.clj' gets auto-loaded
                    :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
@@ -31,8 +31,8 @@
                                   [org.clojure/java.classpath "0.2.2"]
                                   [org.clojure/data.json "0.2.5"]
                                   [org.clojure/tools.nrepl "0.2.7"]
-                                  [pe-datomic-testutils "0.0.1"]
-                                  [pe-rest-testutils "0.0.1"]
+                                  [pe-datomic-testutils "0.0.2"]
+                                  [pe-rest-testutils "0.0.2"]
                                   [org.clojure/tools.nrepl "0.2.7"]
                                   [ring-server "0.3.1"]
                                   [ring-mock "0.1.5"]]}}
